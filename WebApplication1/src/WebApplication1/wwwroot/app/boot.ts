b781bfb858b6fab34b04fce78b7b732a9bc837b4
jci-cpined <david.pine-ext@jci.com>
2016-02-25 07:03:53 -0600

    Added missing .ts file, deleted .map and .js generated files and cleaned up package/project.json

---
+++ WebApplication1/src/WebApplication1/wwwroot/app/boot.ts
@@ -0,0 +1,7 @@
+﻿import {bootstrap}    from "angular2/platform/browser"
+import {ROUTER_PROVIDERS} from "angular2/router";
+import {HTTP_PROVIDERS} from "angular2/http";
+
+import {AppComponent} from "./app.component"
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
